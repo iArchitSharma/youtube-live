e67b23d17f930bfe6949e7b2e1446912806f5c9c
Archit Sharma <74408634+iArchitSharma@users.noreply.github.com>
2025-03-03 21:30:52 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer');
   // Set viewport to match Xvfb resolution
   await page.setViewport({ width: 1280, height: 720 });
   // Replace with the URL you want to stream
-  await page.goto('https://example.com');
+  await page.goto('https://www.wwe.com/');
   // Keep the browser open indefinitely (or until the action is cancelled)
   await new Promise(resolve => {});
 })();
